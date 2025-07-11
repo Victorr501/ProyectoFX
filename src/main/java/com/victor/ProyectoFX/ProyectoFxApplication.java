cc91820600ba941ce90e59384cc2b8150b98991c
victor <vrubinr501@gmail.com>
2025-07-11 16:05:23 +0200

    Initial commit

---
+++ src/main/java/com/victor/ProyectoFX/ProyectoFxApplication.java
@@ -0,0 +1,13 @@
+package com.victor.ProyectoFX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/ProyectoFX/ProyectoFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.ProyectoFX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
